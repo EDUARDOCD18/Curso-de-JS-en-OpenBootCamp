faabb0e52111c07373118a65d2480187b08d6bf6
eduardocd18 <javierecd@gmail.com>
2023-06-24 13:45:53 -0400

    Tarea entregada

---
+++ sesion_1_y_2_mi_primer_proyecto_de_node/index.js
@@ -0,0 +1,15 @@
+//Bienvenida
+console.log('Puerta de entrada al proyecto');
+
+/*
+Comentarios multilinea
+Comentarios multilinea
+Comentarios multilinea
+Comentarios multilinea
+*/
+
+//Intermedio
+console.log("Intermedio")
+
+//Despedida
+console.log("Adiós")
