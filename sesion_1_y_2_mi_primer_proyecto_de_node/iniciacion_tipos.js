5256f0baf29cf80141f87c495629a6b91437fafd
eduardocd18 <javierecd@gmail.com>
2023-06-27 21:16:36 -0400

    * CREACIÓN DE RAMA 'sesion-1-2'*
    Clase de tipos primitivos y tipos de variable

---
+++ sesion_1_y_2_mi_primer_proyecto_de_node/iniciacion_tipos.js
@@ -0,0 +1,26 @@
+//tipos primitivos
+
+// number
+4;
+0;
+
+//string
+"hola";
+'hola';
+`hola`;
+
+//boolean
+true;
+false;
+
+//null and undefined
+null;
+undefined;
+
+//null, undefiined, null, 0 => FALSE
+if(0){
+    console.log("cumple")
+}
+else{
+    console.log("no cumple")
+}
