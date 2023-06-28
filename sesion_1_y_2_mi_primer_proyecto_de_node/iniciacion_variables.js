0a505a057a85f42b027ae47580b064526d07236e
eduardocd18 <javierecd@gmail.com>
2023-06-27 21:44:59 -0400

    inicicion de varibles

---
+++ sesion_1_y_2_mi_primer_proyecto_de_node/iniciacion_variables.js
@@ -0,0 +1,37 @@
+var variable;
+let variablelet;
+
+const constante = "CONSTANTE";
+
+var a = 1;
+console.log(a);
+
+a = 3;
+
+console.log(a);
+console.log(constante);
+
+let b = 3;
+console.log(b);
+
+b = 5;
+console.log(b);
+
+var variable = "I'm a variable";
+
+console.log(variable);
+
+for (var i = 0; i < 3; i++) {
+  var variable = "second variable";
+}
+
+console.log(variable);
+
+for (var i = 0; i < 3; i++) {
+  let variablelet = "Variable let";
+}
+
+console.log(variablelet);
+
+
+console.log(typeof variablelet_
