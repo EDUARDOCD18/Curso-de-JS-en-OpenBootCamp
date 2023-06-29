9261e85aea67c78f8b5b17e895e7ac4142ec535a
eduardocd18 <javierecd@gmail.com>
2023-06-29 08:17:52 -0400

    Iniciación notación

---
+++ sesion_1_y_2_mi_primer_proyecto_de_node/iniciacion_notacion.js
@@ -0,0 +1,16 @@
+// Notación
+
+// ; -> Delimitar una línea
+// . -> Acceder a los atributos de un objeto
+// [] -> Para las listas o arrays
+const ar = [1, 2, 3, 4, 5];
+console.log(ar);
+// {} -> Objetos, funciones y estructuras de control
+const movil = {
+  inch: 5,
+  heigth: 5,
+};
+
+console.log(movil.heigth);
+// () -> Funciones
+function suma(a, b) {}
