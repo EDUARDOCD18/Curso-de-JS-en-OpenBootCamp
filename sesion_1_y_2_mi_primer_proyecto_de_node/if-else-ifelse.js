8712b2112b95ded4e7be1c34f7aa729eb2fa12cb
eduardocd18 <javierecd@gmail.com>
2023-07-01 20:34:11 -0400

    Sesion 3 iniciada
    if
    else
    else if
    switch case

---
+++ sesion_1_y_2_mi_primer_proyecto_de_node/if-else-ifelse.js
@@ -0,0 +1,17 @@
+// If else + if else
+
+let nota = 3;
+
+if (nota === 5) {
+  console.log("Aprovado");
+} else if (nota === 4) {
+  console.log("Buen trabajo");
+} else if (nota === 3) {
+  console.log("Mejorable");
+} else if (nota === 2) {
+  console.log("Deficiente");
+} else if (nota === 1) {
+  console.log("Reprobado");
+} else {
+  console.log("Error!");
+}
---
+++ sesion_1_y_2_mi_primer_proyecto_de_node/if-else.js
@@ -0,0 +1,13 @@
+// Bifurcaciones if-else
+let saldo = 120;
+let efectivo = 200;
+
+if (efectivo < saldo) {
+  console.log("Retirar");
+}
+
+if (efectivo < saldo) {
+  console.log("Retirar");
+} else {
+  console.log("Saldo insuficiente");
+}
