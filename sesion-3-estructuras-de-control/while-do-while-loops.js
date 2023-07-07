b300a046a936cdebb7930706dc9343a731157f64
eduardocd18 <javierecd@gmail.com>
2023-07-07 08:07:38 -0400

    while y do while

---
+++ sesion-3-estructuras-de-control/while-do-while-loops.js
@@ -0,0 +1,15 @@
+// while loop
+
+let i = 0;
+let max = 10;
+while (i < max) {
+  console.log(i);
+  i++;
+}
+
+//do while
+i = 0;
+do {
+  console.log("Esto es do-while");
+  i++;
+} while (i < max);
