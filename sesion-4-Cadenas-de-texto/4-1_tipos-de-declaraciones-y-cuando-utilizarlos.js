266ce81bfd1bb50d7ae2cfa17ef394a7dc9fd762
eduardocd18 <javierecd@gmail.com>
2023-07-13 07:30:39 -0400

    Tipos de declaracion de strings y cuando utilizarlos

---
+++ sesion-4-Cadenas-de-texto/4-1_tipos-de-declaraciones-y-cuando-utilizarlos.js
@@ -0,0 +1,30 @@
+// sesión 4 cadenas de texto
+let str = "Hola, soy un texto";
+let figura = '"Soy una figura"';
+let hola = 'Hola "Javier"';
+let simple = "Hola 'hola'";
+
+console.log(str);
+console.log(figura);
+console.log(hola);
+console.log(simple);
+
+// Comillas invertidas
+let str_back = `string con backticks`;
+console.log(str_back);
+
+let nombre = "Javier";
+let saludo = `hola, ${nombre}. Bienvenido`;
+console.log(saludo);
+
+//Plantillas html
+let plantilla = `
+<html>
+    <h1>Título</h1>
+    <p>Párrafo</p>
+</html>`;
+
+console.log(plantilla);
+
+// Introducción de variables en HTML
+let lista_libros = ["Why", "2 segundos", "XD"]
