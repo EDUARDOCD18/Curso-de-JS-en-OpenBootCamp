f3feeb78861ee750fcde5c75d9aedaae4dfeda90
eduardocd18 <javierecd@gmail.com>
2023-07-24 08:58:11 -0400

    5.2- Operaciones arigméticas y de redondeo

---
+++ sesion-5-números-en-js/5-2_Operaciones_arigmeticas_y_de_redondeo.js
@@ -0,0 +1,34 @@
+// Principales operaciones arigméticas
+let a = 3.5;
+let b = 4.8;
+
+// Suma
+console.log(a + b);
+
+// Resta
+console.log(a - b);
+
+// Multiplicación
+console.log(a * b);
+
+// División
+console.log(a / b);
+
+// Representacióm de los números en cadenas de texto
+console.log(typeof a);
+let a_s = a.toString();
+
+console.log(a_s);
+console.log(typeof a_s);
+
+// Redondeo de números
+let c = 3.3
+let d = c*3
+
+console.log(d);
+// .toFixed()
+console.log(d.toFixed(2));
+
+// .toPrecisition()
+let e = 3.54323423875
+console.log(e.toPrecision(7));
