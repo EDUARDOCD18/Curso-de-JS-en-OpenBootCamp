af632636a8d85c325a050d2b20bd9c33fb86d1b4
eduardocd18 <javierecd@gmail.com>
2023-07-24 08:40:37 -0400

    5.1- Números y precisión en JS

---
+++ sesion-5-números-en-js/5-1_numeros_y_precision.js
@@ -0,0 +1,15 @@
+// Sesión 5 números
+
+// declaración de variables numéricas, enteros y decimales
+let a = 5;
+console.log(a);
+
+let b = 0.1;
+console.log(b);
+
+// Precisión
+let c = 0.2;
+console.log(c + b);
+
+// truco para obtener los valores reales
+console.log(Math.round((0.1 + 0.2) *100 ) /100);
