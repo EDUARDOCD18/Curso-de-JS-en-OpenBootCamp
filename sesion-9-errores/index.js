6808e532e6a3e83df51d9763906377f6c12b58c3
eduardocd18 <javierecd@gmail.com>
2023-08-14 16:55:49 -0400

    9.2- Gestión de logs en NodeJS

---
+++ sesion-9-errores/index.js
@@ -0,0 +1,7 @@
+const logger = require('./logger')
+
+// logger.log("Hola");
+logger.info("Mensaje informatico");
+logger.warn("Advertencia");
+logger.error("Error");
+logger.debug("Mensaje de debug");
