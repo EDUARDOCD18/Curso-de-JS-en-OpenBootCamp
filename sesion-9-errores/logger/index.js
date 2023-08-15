156537c882baff8acbc33debfc8bacc64db0fc89
eduardocd18 <javierecd@gmail.com>
2023-08-14 21:07:58 -0400

    Correción simple en /logger/index.js

--- sesion-9-errores/logger/index.js
+++ sesion-9-errores/logger/index.js
@@ -9,7 +9,7 @@ const logger = winston.createLogger({
     // - Write all logs with importance level of `error` or less to `error.log`
     // - Write all logs with importance level of `info` or less to `combined.log`
     //
-    new winston.transport.Console(),
+    new winston.transports.Console(),
     new winston.transports.File({ filename: "error.log", level: "error" }),
     new winston.transports.File({ filename: "combined.log" }),
   ],
