a4a92a93250af1c2a823301d1efb375ed704b4b6
eduardocd18 <javierecd@gmail.com>
2023-08-29 12:52:32 -0400

    13.3- Reglas_temporales_y_scrips_para_ejecución_de_ESLint_en_nuestro_código

--- sesion-13-eslint/eslint-npm/index.js
+++ sesion-13-eslint/eslint-npm/index.js
@@ -1,4 +1,18 @@
 // Este es el archivo a usar
 
 const nombre = "Javier"
+
+//En esta línea quiero tener comillas dobles, desacivar eslint
+
+/* eslint-disable */
 const persona = 'Wili'
+/* eslint-enable */
+
+const nuevo = 'Hola' // eslint-disable-line quotes
+
+
+/* eslint-disable-next-line indent */
+    const nuevo2= "Más xd"
+
+console.log("Hola")
+const nombre3 = "HOLA COCACOLA"
