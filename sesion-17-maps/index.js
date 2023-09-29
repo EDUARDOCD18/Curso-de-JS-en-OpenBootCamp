0555a9e2ef99c7d44ac80e072602b99d28e28867
eduardocd18 <javierecd@gmail.com>
2023-09-29 07:41:08 -0400

    Video 62 terminado. Arreglar la API para que funcione el mapa

--- sesion-17-maps/index.js
+++ sesion-17-maps/index.js
@@ -1,18 +1,43 @@
+let marker, map;
+
 function initMap() {
-    console.log("mapa");
-    const posicion = {
-        lat: -25.363,
-        lng: 131.044
-    }
+  console.log("mapa");
+  const posicion = {
+    lat: -25.363,
+    lng: 131.044,
+  };
 
-    const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 4,
-        center: posicion
-    })
+  map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: posicion,
+  });
 
-    const marker = new google.maps.Marker({
-        position: posicion,
-        map,
-        title: "Posición Inicial"
-    })
+  const marker = new google.maps.Marker({
+    position: posicion,
+    map,
+    title: "Posición Inicial",
+  });
+}
+
+function geoPosicion() {
+  if (navigator.geolocation) {
+    const geoLoc = navigator.geolocation;
+    const options = { timeout: 60000 };
+    const watchPos = geoLoc.watchPosition(centraMapa, onError, options);
+  }
+}
+
+function centraMapa(posicion) {
+  const nuevaPos = {
+    lat: position.coords.latitude,
+    lng: position.coords.longtude,
+  };
+  console.log(nuevaPos);
+  marker.setPosition(nuevaPos)
+  map.setCenter(nuevaPos)
+}
+
+function onError(error) {
+  console.log("Error producido y se ha gestionado");
+  console.error(error);
 }
